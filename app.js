99b518331794b5856d396dd051e4df2bc1f06032
Karl Sigel <heykarl@gmail.com>
2021-08-17 17:22:03 -0700

    added compareNumbers function

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 // import functions and grab DOM elements
-const numberInput = document.querySelector('#number-input');
+const guessInput = document.querySelector('#guess-input');
 const submitBtn = document.querySelector('#submit-btn');
 const triesRemaining = document.querySelector('#tries-remaining');
 const tooHighOrLow = document.querySelector('#too-high-or-low');
 const winLose = document.querySelector('#win-or-lose');
+
 // initialize global state
-let aRandomNumer = 0;
-let triesRemaining = 4;
+let correctNumber = Math.ceil(Math.random() * 20);
+// let howManyLeft = 4;
+
+console.log(correctNumber);
+// console.log(howManyLeft);
+
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+submitBtn.addEventListener('click', () => {
+    const guess = guessInput.value;
+
+    console.log(guess);
+});
+
+function compareNumbers(guess, correctNumber) {
+    if (guess === correctNumber) return 0;
+    if (guess < correctNumber) return -1;
+    if (guess > correctNumber) return 1;
+}
